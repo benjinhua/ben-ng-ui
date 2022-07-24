2b22c8f4bf77e88a49d27495d8b7c295b67bd7cb
benjinhua <benjinhua66@gmail.com>
2022-07-24 22:49:01 +0800

    // dom操作以及@viewchild   // angular执行css3动画

--- src/app/study/day4/day4.component.ts
+++ src/app/study/day4/day4.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-day4',
   templateUrl: './day4.component.html',
-  styleUrls: ['./day4.component.sass']
+  styleUrls: ['./day4.component.sass'],
 })
 export class Day4Component implements OnInit {
+  // dom操作以及@viewchild
+  // angular执行css3动画
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
