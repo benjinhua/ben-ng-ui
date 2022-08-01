dd4a1b68f7753a639af64e108b143eb1539dfd65
benjinhua <benjinhua66@gmail.com>
2022-08-01 14:39:02 +0800

    day8：请求数据

--- src/app/study/day8/day8.component.ts
+++ src/app/study/day8/day8.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { RequestService } from "src/app/services/request.service";
 
 @Component({
-  selector: 'app-day8',
-  templateUrl: './day8.component.html',
-  styleUrls: ['./day8.component.sass']
+  selector: "app-day8",
+  templateUrl: "./day8.component.html",
+  styleUrls: ["./day8.component.sass"],
 })
 export class Day8Component implements OnInit {
-
-  constructor() { }
+  constructor(public request: RequestService) {}
 
   ngOnInit(): void {
+    var streamInterval = this.request.getRxjsIntervalData();
+    streamInterval.subscribe((data) => {
+      console.log(data);
+    });
   }
-
 }
